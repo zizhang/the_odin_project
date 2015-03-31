675b50da1955a98bc9680082bc6c7d4679cb476a
zizhang <zhang_ziwei@hotmail.com>
2015-03-31 16:25:32 -0700

    New Grid Button

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,8 +1,12 @@
 var $cell = $('<div />', {class:'cell'});
 var $row = $('<div />', {class: 'row'});
+var $container = $('<div />', {class: 'container'});
+var rows = 16;
+var cols = 16;
 
 $(document).ready(function() {
-	createGrid(10, 10);
+	createGrid(rows, cols);
+
 	$('.cell').hover(function() {
 		$(this).addClass('highlight');
 	});
@@ -10,16 +14,36 @@ $(document).ready(function() {
 	$('#clearBtn').on('click', function() {
 		$('.cell').removeClass('highlight');
 	});
+
+	$('#newGridBtn').on('click', function() {
+		newRows = prompt("Enter new grid size (1-64):");
+		newCols = newRows;
+		if(newRows < 0 || newRows > 64) {
+			alert('Invalid grid size!');
+			newRows = 16;
+			newCols = 16;
+		} else {
+			newCols = newRows;
+			alert("new row " + newRows + " new col " + newCols);
+		}
+		// Clear elements of previous grid
+		$row.empty();
+		$container.empty();
+		createGrid(newRows, newCols);
+		$('.cell').hover(function() {
+			$(this).addClass('highlight');
+		});
+	});
 });
 
 function createGrid(numRows, numCols) {
-	for(var i = 0; i < numRows; i++) {
+	for(var i = 0; i < numCols; i++) {
 		$row.append($cell.clone());
 	}
 
-	for(var i = 0; i < numCols; i++) {
-		$('#container').append($row.clone());
+	for(var i = 0; i < numRows; i++) {
+		$container.append($row.clone());
 	}
 
-	
+	$('body').append($container);
 }
